39f4381668713bce7a6386603eac275888021665
TomaszMoroz <tmoroz688@gmail.com>
2025-04-10 11:25:31 +0200

    Add files via upload

---
+++ healet-html/js/custom.js
@@ -0,0 +1,77 @@
+// Scroll to the top when the button is clicked
+function scrollToTop() {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
+function scrollToBottom() {
+  // Scroll to Bottom Button Functionality
+  const scrollToBottomBtn = document.getElementById("scrollToBottomBtn");
+
+  // Scroll to the bottom when the button is clicked
+  scrollToBottomBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: document.body.scrollHeight, // Scroll to the bottom of the page
+      behavior: "smooth",
+    });
+  });
+}
+
+scrollToTop();
+scrollToBottom();
+
+// to get current year
+function getYear() {
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// overlay menu
+function openNav() {
+  document.getElementById("myNav").classList.toggle("menu_width");
+  document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
+}
+
+/** google_map js **/
+
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(40.712775, -74.005973),
+    zoom: 18,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+// lightbox gallery
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+function openTab(event, tabId) {
+  // Hide all tab content
+  const tabContents = document.querySelectorAll(".tab-content");
+  tabContents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  // Remove active class from all buttons
+  const tabButtons = document.querySelectorAll(".tab-btn");
+  tabButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  // Show the selected tab content
+  document.getElementById(tabId).classList.add("active");
+
+  // Add active class to the clicked button
+  event.currentTarget.classList.add("active");
+}
